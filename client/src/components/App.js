02d373fe2508eb884de85f1c57e4c1a340b66cd2
Zhiyuan Ma <631253076@qq.com>
2022-01-04 19:04:44 -0500

    react reducer, route, css & redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {connect} from "react-redux";
+import * as actions from "../actions";
 
-const App = () => {
-    return (<div>Hi There</div>);
-};
+import Header from "./Header";
+import {fetchUser} from "../actions";
 
-export default App;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div className={"container"}>
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <Routes>
+                            <Route path="/" element={<Landing/>}/>
+                            <Route path="/surveys" element={<Dashboard/>}/>
+                            <Route path="/surveys/new" element={<SurveyNew/>}/>
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default connect(null, actions)(App);
