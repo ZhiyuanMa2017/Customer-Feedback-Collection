d6235862837d6c8c95a31cf4ac28b508d8f3ee8e
Zhiyuan Ma <631253076@qq.com>
2022-01-06 01:31:24 -0500

    payment

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
     renderContent() {
@@ -8,12 +9,12 @@ class Header extends Component {
             case null:
                 return;
             case false:
-                return (
-                    <li>
-                        <a href={"/auth/google"}>Login With Google</a>
-                    </li>);
+                return <li><a href={"/auth/google"}>Login With Google</a></li>;
             default:
-                return (<li><a href={"/api/logout"}>Logout</a></li>);
+                return [
+                    <li key={"Payments"}><Payments/></li>,
+                    <li key={"Logout"}><a href={"/api/logout"}>Logout</a></li>
+                ];
         }
     }
 
