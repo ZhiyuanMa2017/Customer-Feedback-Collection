75ae02a2ecabc90f9ae49c7a7cd141cd9ab5b8ec
Zhiyuan Ma <631253076@qq.com>
2022-01-10 03:44:25 -0500

    form frontend part 2

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,44 @@
+import _ from "lodash";
+import React from "react";
+import {connect} from "react-redux";
+import formFields from "./formFields";
+import {useNavigate} from "react-router-dom";
+import * as actions from "../../actions";
+
+const SurveyFormReview = ({onCancel, formValues, submitSurvey}) => {
+    const navigate = useNavigate();
+
+    const reviewFields = _.map(formFields, ({name, label}) => {
+        return (
+            <div key={name}>
+                <label>{label}</label>
+                <div>
+                    {formValues[name]}
+                </div>
+            </div>
+
+        );
+    });
+    return (
+        <div>
+            <h5>Please confirm your entries</h5>
+            {reviewFields}
+            <button className={"yellow darken-3 btn-flat"} onClick={onCancel}>
+                Back
+            </button>
+            <button className={"green  btn-flat right"} onClick={() => {
+                submitSurvey(formValues);
+                navigate("/surveys");
+            }}>
+                Send Survey
+                <i className={"material-icons right"}>email</i>
+            </button>
+        </div>
+    )
+};
+
+function mapStateToProps(state) {
+    return {formValues: state.form.surveyForm.values};
+}
+
+export default connect(mapStateToProps, actions)(SurveyFormReview);
